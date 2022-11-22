bfd510a7854bf8efa6e9889c1e2f75afc0c4b411
jordorob <jordanjkroberts@gmail.com>
2022-11-21 22:53:13 -0800

    Jordan R Done Dashboard

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 
+def animation(genre):
+    if 'Animation' in genre:
+        return "yes"
+    else:
+        return "no"
 def load_and_process(csvpath):
     df1= pd.read_csv(csvpath)
-    df1=(
-        .copy().drop(['title','directors','camera_format','negative_format','budget_source','genres'], axis=1)
+    df1["animated"]=df1.apply (lambda row: animation(row['genres']), axis=1)
+    df1=(df1
+        .copy().drop(['directors','camera_format','negative_format','budget_source', 'genres'], axis=1)
         .dropna(axis=0)
-        .query("film_type==['D','F']")
+        .query("film_type==['D','F','D|F']")
         .query("budget>0")
     )
-    df1["film_type"]=df1["film_type"].str.replace('F',"Film")
-    df1["film_type"]=df1["film_type"].str.replace('D',"Digital")
     return(df1)
