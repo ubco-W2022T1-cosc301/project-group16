23739a17eb5a494f8719054c84d6ef0299c31872
Talon Lusk <talonstevelusk@gmail.com>
2022-11-23 17:44:02 -0800

    Working.

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -16,16 +16,17 @@ def load_and_process(url_or_path_to_csv_file):
         pd.read_csv(url_or_path_to_csv_file)
         .rename(columns={"production_year": "year"})
         .merge(pd.read_csv("../data/raw/top_movies.csv"), on=['id'], how="left", suffixes=('', '_y'))
-        .loc[:, ["director", "negative_format", "year", "budget"]]
+        .loc[:, ["director", "negative_format", "year", "budget", "genres", "film_type"]]
     )
     
     # Method Chain 2 (Create new columns, drop others, and do processing)
-
     df2 = (
         df1
         .dropna()
         .reset_index(drop=True)
         .assign(negative_format=df1.negative_format.str.split("|")).explode('negative_format')
+        .assign(film_type=df1.film_type.str.split("|")).explode('film_type')
+        .assign(genres=df1.genres.str.split("|")).explode('genres')
         .reset_index(drop=True)
         .sort_values("year", ascending=False)
     )
