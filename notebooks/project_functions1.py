df3935e324afaff81a3269e63b25a24914b0c5fd
Talon Lusk <talonstevelusk@gmail.com>
2022-11-29 22:43:23 -0800

    Working.

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -22,15 +22,13 @@ def load_and_process(url_or_path_to_csv_file):
     # Method Chain 2 (Create new columns, drop others, and do processing)
     df2 = (
         df1
-        .dropna()
-        .reset_index(drop=True)
         .assign(negative_format=df1.negative_format.str.split("|")).explode('negative_format')
         .assign(film_type=df1.film_type.str.split("|")).explode('film_type')
         .assign(genres=df1.genres.str.split("|")).explode('genres')
+        .sort_values("year", ascending=True)
         .reset_index(drop=True)
-        .sort_values("year", ascending=False)
     )
-    
+
     # Make sure to return the latest dataframe
     return df2 
 
@@ -52,3 +50,10 @@ def get_popular_formats(df_clean):
         .reset_index(drop=True)
     )
     return i
+# Function for Mean Budget
+def camera_mean_budget(df_clean):
+    df_d = df_clean.groupby(by=['negative_format']).mean()
+    df_d = df_d.dropna()
+    df_d = df_d.reset_index()
+    df_d = df_d[df_d["budget"]>100869747]
+    return df_d
